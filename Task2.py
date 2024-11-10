0623285d61cf5855644d34f43331e0bf7b8172ef
Osama <xekkon6@gmal.com>
2024-11-10 20:31:28 +0200

    Assigment2

---
+++ Task2.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+apikey = "3cc565570a202a7edb9124fc824af2bd"
+
+def open_weather(city):
+    server = "https://api.openweathermap.org/"
+    request = f"{server}data/2.5/weather?q={city}&appid={apikey}"
+    response = requests.get(request)
+    return response.status_code, response.json()
+
+def temp_celsius(kelvin):
+    return kelvin - 273.15
+
+city = input("Enter city: ")
+status_code, weather_data = open_weather(city)
+
+if status_code == 200:
+    kelvin_temp = weather_data['main']['temp']
+    celsius_temp = temp_celsius(kelvin_temp)
+    print(f"The temperature in {city} is {celsius_temp:.2f}°C.")
+else:
+    print("Error fetching data:", weather_data.get("message", "Unknown error"))
